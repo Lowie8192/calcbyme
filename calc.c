fdd633a68dc5ed74e08d4c3008281cb8e7357a36
Lowie <lwgeebelgium@gmail.com>
2021-01-07 20:41:23 +0100

    Init

---
+++ calc.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main()
+{
+	printf("Hi, and welcome to this calc.\n");
+	
+	double firstNumber;
+	printf("Enter your first number: ");
+	scanf("%lf", &firstNumber);
+
+	char op;
+	printf("Enter your operator: ");
+	scanf(" %c", &op);	
+
+	double secondNumber;
+	printf("Enter your second number: ");
+	scanf("%lf", &secondNumber);
+	
+	if(op == '+'){
+		printf("%f\n", firstNumber + secondNumber);
+	}
+	else if(op == '-'){
+		printf("%f\n", firstNumber - secondNumber);
+	}
+	else if(op == '*'){
+		printf("%f\n", firstNumber * secondNumber);
+	}
+	else if(op == '/'){
+		printf("%f\n", firstNumber / secondNumber);
+	}
+	else {
+		printf("Something went wrong...\n");	
+	}
+	
+	printf("This (awesome) program is made by Lowie8192!");	
+
+	return 0;
+}
